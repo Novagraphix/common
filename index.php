ea879dd6fa2f2d42514c4afb6af947e8575c7edd
Stephan Jess <s.jess@novagraphix.de>
2013-01-14 11:12:12 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="de-DE">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<link rel="stylesheet" href="css/style.css" media="all">
+
+<body>
+<div id="page-">
+    <div id="header">header</div>
+    <div id="content">content</div>
+    <div id="footer">footer</div>
+</div>
+</body>
+</html>
