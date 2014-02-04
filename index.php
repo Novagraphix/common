80acf44e237f2f8ff4f79e62dd0b78c80cacb2c6
Stephan Jess <s.jess@novagraphix.de>
2014-02-04 09:02:01 +0100

    replaced old stuff

--- index.php
+++ index.php
@@ -1,34 +1,49 @@
 <?php
-	require_once 'inc/Twig/Autoloader.php';
-	Twig_Autoloader::register();
 
-	$loader = new Twig_Loader_Filesystem('templates');
-	$twig = new Twig_Environment($loader, array(
-		'cache' => 'cache',
-		'auto_reload' => true
-	));
+/**
+ * Variablen
+ */
 
-	$base = 'http://172.31.1.61/testumgebung/an_ihrer_stelle/';
+$base = ""; //http://www.film.de
+$is_base = false;
+$is_skel = false;
+
+/**
+ * Ende
+ */
+
+require_once 'inc/Twig/Autoloader.php';
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader, array(
+	'cache' => 'cache',
+	'auto_reload' => true
+));
+
+if($is_base) {
 	if ($_SERVER["SERVER_NAME"]=='localhost'||$_SERVER["SERVER_NAME"]=='127.0.0.1') {
 		$base = 'http://'.$_SERVER["SERVER_NAME"].'/repositories/common/';
 	}
+}
 
-	$page = "index";
-	if (isset($_GET["page"])) {
-		$page = $_GET["page"];
-		if ($page == '') {
-			$page = 'index';
-		}
+$page = "index";
+if (isset($_GET["page"])) {
+	$page = $_GET["page"];
+	if ($page == '') {
+		$page = 'index';
 	}
+}
 
-	$tplfile = '/pages/index.html';
-	if (isset($_GET["page"])) {
-		$tplfile = '/pages/'.$page.'.html';
-	}
+$tplfile = '/pages/index.html';
+if (isset($_GET["page"])) {
+	$tplfile = '/pages/'.$page.'.html';
+}
 
-	echo $twig->render($tplfile, array(
-		'page' => $page,
-		'base' => $base
-	));
+echo $twig->render($tplfile, array(
+	'page' => $page,
+	'base' => $base,
+	'is_skel' => $is_skel
+));
 
 ?>
